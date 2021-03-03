f22075e0a267e822c6c50e865aa332354f536afc
Brian Miller <brian@parameter1.com>
2021-03-03 15:57:10 -0600

    setup homepage and add nativeX placements

--- sites/bizbash.com/config/native-x.js
+++ sites/bizbash.com/config/native-x.js
@@ -4,7 +4,23 @@ const config = new NativeXConfiguration('https://bizbash.native-x.parameter1.com
 
 config
   .setAliasPlacements('default', [
+    { name: 'primary', id: '5d4af41d2ab3e700014e2dd6' },
     { name: 'load-more', id: '5d4af41d2ab3e700014e2dd6' },
+  ])
+  .setAliasPlacements('catering', [
+    { name: 'primary', id: '5d4b04513bb2db00018cfa1a' },
+  ])
+  .setAliasPlacements('style-decor', [
+    { name: 'primary', id: '5d4b04619f69b200013ab0f4' },
+  ])
+  .setAliasPlacements('production-strategy', [
+    { name: 'primary', id: '5d4b04769f69b200013ab109' },
+  ])
+  .setAliasPlacements('local-venues-destinations', [
+    { name: 'primary', id: '5d4b04833bb2db00018cfa65' },
+  ])
+  .setAliasPlacements('bizbash-lists', [
+    { name: 'primary', id: '5d4b04963bb2db00018cfa7b' },
   ]);
 
 module.exports = config;
