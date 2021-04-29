f6bee79052c7601c82bb228ad425e00a48237406
Brian Miller <brian@parameter1.com>
2021-04-29 12:22:06 -0500

    initial port of all the venue|supplier directory pages
    @todo finish porting styles, but they  should be functionally ported over.

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,9 +1,11 @@
+import DefaultTheme from '@parameter1/base-cms-marko-web-theme-default/browser';
 import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
 
 const ImageSlider = () => import(/* webpackChunkName: "common-image-slider" */ './image-slider.vue');
 const ContactUsForm = () => import(/* webpackChunkName: "common-contact-us" */ './contact-us-form.vue');
 
 export default (Browser) => {
+  DefaultTheme(Browser);
   Inquiry(Browser);
 
   // @todo this should be removed once contact us is moved to core.
