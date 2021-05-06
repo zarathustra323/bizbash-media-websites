3ed4cf1982af9db8c4c7153e2b22b80b55554467
Brian Miller <brian@parameter1.com>
2021-05-06 10:31:41 -0500

    add social share to content pages

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,6 +1,7 @@
 import DefaultTheme from '@parameter1/base-cms-marko-web-theme-default/browser';
 import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
 import RevealAd from '@parameter1/base-cms-marko-web-reveal-ad/browser';
+import SocialSharing from '@parameter1/base-cms-marko-web-social-sharing/browser';
 import InquiryForm from './inquiry-form.vue';
 
 const ImageSlider = () => import(/* webpackChunkName: "common-image-slider" */ './image-slider.vue');
@@ -10,6 +11,7 @@ export default (Browser) => {
   DefaultTheme(Browser);
   Inquiry(Browser, { component: InquiryForm });
   RevealAd(Browser);
+  SocialSharing(Browser);
 
   // @todo this should be removed once contact us is moved to core.
   Browser.register('GlobalContactUsForm', ContactUsForm);
