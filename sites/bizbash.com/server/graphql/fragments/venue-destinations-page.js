f40bb20dfaa52d669e6ca5acfe3d1901cf27d3fc
Shinsina <shinsina@att.net>
2021-05-11 15:34:15 -0500

    Updated Venue Destinations template(s)

---
+++ sites/bizbash.com/server/graphql/fragments/venue-destinations-page.js
@@ -0,0 +1,33 @@
+const gql = require('graphql-tag');
+
+module.exports = gql`
+fragment VenueDestinationsFragment on WebsiteSection {
+  id
+  name
+  description
+  hierarchy {
+    id
+    alias
+    name
+  }
+  children(input: { pagination: { limit: 0 } }) {
+    edges {
+      node {
+        id
+        alias
+        name
+        children(input: { pagination: { limit: 0 } }) {
+          edges {
+            node {
+              id
+              alias
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
+`;
