27903c4ca8d439ee9d78939e0f70f0a4d1e35242
Brian Miller <brian@parameter1.com>
2021-05-14 10:32:54 -0500

    dynamically load vue components

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -2,10 +2,10 @@ import DefaultTheme from '@parameter1/base-cms-marko-web-theme-default/browser';
 import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
 import RevealAd from '@parameter1/base-cms-marko-web-reveal-ad/browser';
 import SocialSharing from '@parameter1/base-cms-marko-web-social-sharing/browser';
-import InquiryForm from './inquiry-form.vue';
 
-const ImageSlider = () => import(/* webpackChunkName: "common-image-slider" */ './image-slider.vue');
-const ContactUsForm = () => import(/* webpackChunkName: "common-contact-us" */ './contact-us-form.vue');
+const ImageSlider = () => import(/* webpackChunkName: "global-image-slider" */ './image-slider.vue');
+const InquiryForm = () => import(/* webpackChunkName: "global-inquiry-form" */ './inquiry-form.vue');
+const ContactUsForm = () => import(/* webpackChunkName: "global-contact-us" */ './contact-us-form.vue');
 
 export default (Browser) => {
   DefaultTheme(Browser);
