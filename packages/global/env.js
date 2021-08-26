2903a38d7e27b70a1b945597ddc3d18f25859afb
Brian Miller <brian@parameter1.com>
2021-08-26 14:12:41 -0500

    Add RECAPTCHA_SITE_KEY to required env vars

--- packages/global/env.js
+++ packages/global/env.js
@@ -5,5 +5,6 @@ const { nonemptystr } = validators;
 // @todo This should be removed once contact us is moved to core and the mailer service is created.
 module.exports = cleanEnv(process.env, {
   RECAPTCHA_SECRET_KEY: nonemptystr({ desc: 'The Recaptcha secret key.' }),
+  RECAPTCHA_SITE_KEY: nonemptystr({ desc: 'The Recaptcha site key.' }),
   SENDGRID_API_KEY: nonemptystr({ desc: 'An API key for sending email via SendGrid.' }),
 });
