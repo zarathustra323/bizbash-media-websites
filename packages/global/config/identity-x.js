7aba29ef119597c727e30e6c11743239d24ed99d
Jacob Bare <jacob@parameter1.com>
2021-11-12 07:25:17 -0600

    Upgrade identity-x package to 2.52.2

--- packages/global/config/identity-x.js
+++ packages/global/config/identity-x.js
@@ -1,16 +1,27 @@
 const IdentityXConfiguration = require('@parameter1/base-cms-marko-web-identity-x/config');
+const { get } = require('@parameter1/base-cms-object-path');
 const newrelic = require('newrelic');
+
+const { log } = console;
 
 module.exports = ({
   appId,
   apiToken = process.env.IDENTITYX_API_TOKEN,
   requiredServerFields,
+  requiredClientFields,
 } = {}) => {
   const config = new IdentityXConfiguration({
     appId,
     apiToken,
     requiredServerFields,
-    onHookError: newrelic.noticeError.bind(newrelic),
+    requiredClientFields,
+    onHookError: (e) => {
+      if (process.env.NODE_ENV === 'development') {
+        log('ERROR IN IDENTITY-X HOOK', e);
+        if (e.networkError) log('Network Error', get(e, 'networkError.result.errors.0'));
+      }
+      newrelic.noticeError(e);
+    },
   });
   return config;
 };
