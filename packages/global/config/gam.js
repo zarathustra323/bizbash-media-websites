cf6c3d5af6507861c72c979e99b68705e2352980
Brandon Krigbaum <brandonbk@gmail.com>
2022-12-03 07:59:20 -0600

    Update gam account id

--- packages/global/config/gam.js
+++ packages/global/config/gam.js
@@ -1,7 +1,7 @@
 const GAMConfiguration = require('@parameter1/base-cms-marko-web-gam/config');
 
 module.exports = ({
-  accountId = '6407152',
+  accountId = '21802476813',
   basePath,
 } = {}) => {
   const config = new GAMConfiguration(accountId, { basePath });
