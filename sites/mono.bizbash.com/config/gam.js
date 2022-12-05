a52db5bb633b0769718af98dd688f9d96ba8468d
Brandon Krigbaum <brandonbk@gmail.com>
2022-12-04 19:34:58 -0600

    Adjust gam config

---
+++ sites/mono.bizbash.com/config/gam.js
@@ -0,0 +1,32 @@
+const configureGAM = require('@bizbash-media/package-global/config/gam');
+
+const config = configureGAM({ basePath: '' });
+
+config.lazyLoad = {
+  enabled: true, // set to true to enable lazy loading
+  fetchMarginPercent: 100, // fetch ad when one viewport away
+  renderMarginPercent: 50, // render ad when half viewport away
+  mobileScaling: 2, // double these on mobile
+};
+
+config.setAliasAdUnits('default', [
+  { name: 'lb-sticky-bottom', templateName: 'LB-STICKY-BOTTOM', path: 'default/lb1' },
+  { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'default/lb1' },
+  { name: 'rotation', templateName: 'ROTATION', path: 'rotation' },
+  { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: 'rotation' },
+  { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'rotation' },
+  // Below based on enableRevealAd
+  // { name: 'reskin', path: 'reskin' },
+]);
+
+const aliases = [];
+
+aliases.forEach(alias => config.setAliasAdUnits(alias, [
+  { name: 'leaderboard', templateName: 'LEADERBOARD', path: `${alias}-leaderboard` },
+  { name: 'rotation', templateName: 'ROTATION', path: `${alias}-rotation` },
+  { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: `${alias}-rotation` },
+  { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: `${alias}-rotation` },
+]));
+
+
+module.exports = config;
