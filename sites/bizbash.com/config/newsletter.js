8350a3eea80671b9fb494c09e061ca1cb4452799
Brandon Krigbaum <brandonbk@gmail.com>
2022-12-15 15:01:24 -0600

    Update newsletter config

--- sites/bizbash.com/config/newsletter.js
+++ sites/bizbash.com/config/newsletter.js
@@ -1,19 +1,24 @@
-const baseConfig = {
-  action: '/user/subscribe',
-  hiddenInputs: [],
+
+const defaults = {
+  // disabled: process.env.DISABLE_IDX_NEWSLETTER_SIGNUP === 'true',
+  name: 'Don’t Miss Out',
+  description: 'Ideas, inspiration and the hottest industry events directly to your inbox.',
 };
 
 module.exports = {
+  signupBanner: {
+    ...defaults,
+    name: 'You\'re Invited!',
+    imagePath: 'files/base/pmmi/all/image/static/newsletter-pushdown/pw-iphone.png',
+  },
   signupBannerLarge: {
-    ...baseConfig,
-    name: 'Don’t Miss Out',
-    description: 'Ideas, inspiration and the hottest industry events directly to your inbox.',
+    ...defaults,
   },
   signupFooter: {
-    ...baseConfig,
+    ...defaults,
+    action: '/user/subscribe',
     colspan: 5,
     enable: true,
     name: 'Stay Connected',
-    description: 'Ideas, inspiration and the hottest industry events directly to your inbox.',
   },
 };
