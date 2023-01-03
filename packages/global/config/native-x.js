72dae63673ea50760cb389244bd8e496b2fd3a18
Jessica Wade <jessica.wade@parameter1.com>
2023-01-03 08:12:40 -0600

    Fix native-x uri

--- packages/global/config/native-x.js
+++ packages/global/config/native-x.js
@@ -1,6 +1,6 @@
 const NativeXConfiguration = require('@parameter1/base-cms-marko-web-native-x/config');
 
 module.exports = ({
-  uri = 'https://smg.native-x.parameter1.com',
+  uri = 'https://bizbash.native-x.parameter1.com',
   enabled = false,
 } = {}) => new NativeXConfiguration(uri, { enabled });
