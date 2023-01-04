eb72ec4ab973aca6a6c7a6e7a77072c49e4e947f
Brian Miller <brian@parameter1.com>
2023-01-04 16:12:05 -0600

    Add Missing venue landing page components
    Add custom inquiry form, image slider

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,17 +1,24 @@
 
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
+import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
 import FormLogin from './form-login.vue';
 
 const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
 const PreferenceCenter = () => import(/* webpackChunkName: "braze-preference-center" */ './braze-preference-center.vue');
+const InquiryForm = () => import(/* webpackChunkName: "global-inquiry-form" */ './inquiry-form.vue');
+const ImageSlider = () => import(/* webpackChunkName: "global-image-slider" */ './image-slider.vue');
 
 export default (Browser) => {
+  Inquiry(Browser, { component: InquiryForm });
+
   MonoRail(Browser, {
     enableOmedaIdentityX: false,
     idxArgs: {
       CustomLoginComponent: FormLogin,
     },
   });
+
   Browser.register('ContentMeterTrack', ContentMeterTrack);
   Browser.register('PreferenceCenter', PreferenceCenter);
+  Browser.register('GlobalImageSlider', ImageSlider);
 };
