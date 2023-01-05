32f682354b4153b7aa5e0b8c79a3e1f40e9fe2b2
Brian Miller <brian@parameter1.com>
2023-01-05 08:16:55 -0600

    use local content page graph fragment
    This will included the additional fragment infor for venues & suppliers.

--- sites/bizbash.com/server/routes/content.js
+++ sites/bizbash.com/server/routes/content.js
@@ -1,5 +1,5 @@
 const withContent = require('@bizbash-media/package-global/middleware/with-content');
-const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/content-page');
+const queryFragment = require('@bizbash-media/package-global/graphql/fragments/content-page');
 const contact = require('@bizbash-media/package-global/templates/content/contact');
 const company = require('../templates/content/company');
 const whitepaper = require('../templates/content/whitepaper');
