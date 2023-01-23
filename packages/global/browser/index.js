f3bc31cbeb615cedb00053196dde1f3fd40c9504
Brian Miller <brian@parameter1.com>
2023-01-23 10:18:31 -0600

    Remove braze comps & route

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -3,7 +3,6 @@ import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
 
 const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
-const PreferenceCenter = () => import(/* webpackChunkName: "braze-preference-center" */ './braze-preference-center.vue');
 const InquiryForm = () => import(/* webpackChunkName: "global-inquiry-form" */ './inquiry-form.vue');
 const ImageSlider = () => import(/* webpackChunkName: "global-image-slider" */ './image-slider.vue');
 
@@ -15,6 +14,5 @@ export default (Browser) => {
   });
 
   Browser.register('ContentMeterTrack', ContentMeterTrack);
-  Browser.register('PreferenceCenter', PreferenceCenter);
   Browser.register('GlobalImageSlider', ImageSlider);
 };
