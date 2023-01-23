dc91242e1713f00a69705a2bf52d9cc080c57771
Brian Miller <brian@parameter1.com>
2023-01-23 11:57:57 -0600

    remove preset idX config values

--- packages/global/config/identity-x.js
+++ packages/global/config/identity-x.js
@@ -3,34 +3,14 @@ const newrelic = require('newrelic');
 
 module.exports = ({
   appId,
-  hiddenFields = [
-    'street',
-    'addressExtra',
-    'organization',
-    'organizationTitle',
-    'phoneNumber',
-  ],
-  defaultCountryCode = '',
-  requiredServerFields = [
-    'givenName',
-    'familyName',
-    'city',
-    'regionCode',
-    'countryCode',
-  ],
-  requiredClientFields = [
-    'givenName',
-    'familyName',
-    'city',
-    'regionCode',
-    'countryCode',
-  ],
+  hiddenFields,
+  requiredServerFields,
+  requiredClientFields,
   ...rest
 } = {}) => {
   const config = new IdentityXConfiguration({
     appId,
     apiToken: process.env.IDENTITYX_API_TOKEN,
-    defaultCountryCode,
     hiddenFields,
     requiredServerFields,
     requiredClientFields,
