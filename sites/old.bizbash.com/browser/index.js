9bd3a5393e9a28a8386afdddbe6b5c8d295d4742
Brandon Krigbaum <brandonbk@gmail.com>
2023-02-06 08:32:45 -0600

    Delete old bizbash site

--- sites/old.bizbash.com/browser/index.js
+++
@@ -1,6 +0,0 @@
-import Browser from '@parameter1/base-cms-marko-web/browser';
-import Global from '@bizbash-media/package-shared/browser';
-
-Global(Browser);
-
-export default Browser;
--- sites/old.bizbash.com/server/routes/search.js
+++
@@ -1,5 +0,0 @@
-const template = require('@bizbash-media/package-shared/templates/search');
-
-module.exports = (app) => {
-  app.get('/search', (_, res) => { res.marko(template); });
-};
