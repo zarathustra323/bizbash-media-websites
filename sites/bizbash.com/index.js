8ca7a4cd1097982e691a206e9aa5465dd6492a5c
Jacob Bare <jacob@parameter1.com>
2023-02-25 15:42:24 -0600

    Pass package website config to server

--- sites/bizbash.com/index.js
+++ sites/bizbash.com/index.js
@@ -1,5 +1,6 @@
 const startServer = require('@bizbash-media/package-global/start-server');
 
+const pkg = require('./package.json');
 const routes = require('./server/routes');
 const siteConfig = require('./config/site');
 const coreConfig = require('./config/core');
@@ -8,6 +9,7 @@ const { log } = console;
 
 module.exports = startServer({
   rootDir: __dirname,
+  website: pkg.website,
   coreConfig,
   siteConfig,
   routes,
