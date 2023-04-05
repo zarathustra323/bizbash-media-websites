8bc463f75b77f44e9fde456788e6ee00e1c8ac4a
Brian Miller <brian@parameter1.com>
2023-04-05 08:47:07 -0500

    Correctly override/register inquiry browser component.
    This will require a dep upgrade after https://github.com/parameter1/base-cms/pull/669 is merged and deployed

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,15 +1,13 @@
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
-import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
 
 const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
 const InquiryForm = () => import(/* webpackChunkName: "global-inquiry-form" */ './inquiry-form.vue');
 const ImageSlider = () => import(/* webpackChunkName: "global-image-slider" */ './image-slider.vue');
 
 export default (Browser) => {
-  Inquiry(Browser, { component: InquiryForm });
-
   MonoRail(Browser, {
     enableOmedaIdentityX: false,
+    inquiryArgs: { component: InquiryForm },
   });
 
   Browser.register('ContentMeterTrack', ContentMeterTrack);
